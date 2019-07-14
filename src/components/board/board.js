e512a06dad51211469142b57b4cbcb793c09693b
ndlamini <ndlamini@retrorabbit.co.za>
2019-07-14 11:05:15 +0200

    WIP:add and delete of list working

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,13 +1,55 @@
 import React from "react";
 import List from "../list/list"
+let id=1;
+export const CARD_ACTIONS={ADD:"add",DELETE:"delete",EDIT:"edit",SAVE:"save"}
 
 export default class Board extends React.Component{
     constructor(props) {
         super(props)
         this.state={
-            list:[]
+            list:[
+                {
+                    name:"New list",
+                    id,
+                    cards:[]
+                }
+            ]
         }
     }
+
+    onButtonClick(){
+        let myList=this.state.list
+        id++;
+        myList.push({name:"New list",id,cards:[]})
+        this.setState({list:myList})
+    }
+    removeList(id){
+        let myList=this.state.list
+        let ind=myList.findIndex((item)=>item.id==id)
+        myList.splice(ind,1)
+        this.setState({list:myList})
+    }
+    cardAction(action,listId,cardId){
+        let myList=this.state.list
+        switch (action) {
+            case CARD_ACTIONS.ADD:{
+                let ind=myList.findIndex((item)=>item.id==listId)
+                myList[ind].cards.push({})
+                this.setState({list:myList})                
+                break;
+            } 
+            case CARD_ACTIONS.DELETE:{
+                let ind=myList.findIndex((item)=>item.id==listId)
+                let cInd=myList[ind].cards.findIndex((item)=>item.id==cardId)
+                myList[ind].cards.splice(cInd,1)
+                this.setState({list:myList})                
+                break;
+            }        
+            default:
+                break;
+        }
+    }
+
     render(){
         return(
             <div className="board-view">
@@ -16,10 +58,14 @@ export default class Board extends React.Component{
                     <h3 className="board-name">{this.props.name}</h3>
                 </header>
                 <div className="list">
-                    <div className="list-container" >                    
-                        <List name=""></List>                
+                    <div className="list-container" >
+                        {
+                            this.state.list.map((item,index)=>{
+                                return <List key={index} name={item.name} id={item.id} cards={item.cards} onCardAction={(action,listId,cardId)=>this.cardAction(action,listId,cardId)} onClose={(id)=>this.removeList(id)}></List>
+                            })
+                        }                                 
                     </div>
-                    <div className="add-list-container">
+                    <div className="add-list-container"onClick={()=>this.onButtonClick()}>
                         <span className="add-list" uk-icon="icon: plus-circle"></span>
                         <p className="add-list-text"> <u>Add a list</u> </p>
                     </div>
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { CARD_ACTIONS } from "../board/board"
 
 export default class Card extends React.Component{
     constructor(props) {
@@ -10,12 +11,13 @@ export default class Card extends React.Component{
     render(){
         return(
             <div className="card" onMouseEnter={()=>this.setState({showAction:true})} onMouseLeave={()=>this.setState({showAction:false})}>
-                <textarea className="card-description" ></textarea>
+                <textarea className="card-description" defaultValue={this.props.id}></textarea>
                 {
                     this.state.showAction &&
                     <div className="card-actions">
                         <span className="edit-icon action" uk-icon="icon:pencil"></span>
                         <span className="save-icon action" uk-icon="icon:check"></span>
+                        <span className="delete-icon action" uk-icon="icon:trash" onClick={()=>this.props.onCardAction(CARD_ACTIONS.DELETE,this.props.id)}></span>
                     </div>
                 }
             </div>
